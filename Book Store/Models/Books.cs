dbcc2dc96ec40e4d03d77f4e1dbb4d34061ea57c
mamin98 <mohamedamin1421@gmail.com>
2023-03-09 21:22:12 +0200

    built structure model

---
+++ Book Store/Models/Books.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Book_Store.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = ""; // string.Empty;
+        public string Isbn { get; set; } = "";
+        public decimal Price { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string? Image { get; set; }
+        
+        [InverseProperty("BookItem")]
+        public List<OrderItem> BookItems { get; set; } = new List<OrderItem>();
+
+        // Publisher
+        [ForeignKey("Publisher")]
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; } = new Publisher();
+
+        // Author 
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = new Author();
+
+        // Type/Category
+        [ForeignKey("BookType")]
+        public int TypeId { get; set; }
+        public Type BookType { get; set; } = new Type();
+
+    }
+}
---
+++ Book Store/Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Book_Store.Models
+{
+    public class OrderItem
+    {
+        
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int OrederId { get; set; }
+        public int BookId { get; set; }
+
+        public Orders Order { get; set; } = new Orders();
+        public Books BookItem { get; set; } = new Books();
+    }
+}
